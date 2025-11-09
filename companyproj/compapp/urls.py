2c645eeae369ebb9963e1b0317d4eb6df9dcb572
Omar Alansari <Alansariq81122@hotmail.com>
2025-11-09 21:00:54 +0300

    task done

--- companyproj/compapp/urls.py
+++ companyproj/compapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
+ 
 urlpatterns = [
     path('employeeslist/', views.employee_list, name='employee_list'),
+    path('employee/<int:employee_id>/', views.employee_detail, name='employee_detail')
 ]
+    #spath('employee/<int:employee_id>/, views.employee_list')
--- companyproj/compapp/views.py
+++ companyproj/compapp/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from .models import Employee
+from .models import Employee, Project
 
+ 
 # Create your views here.
-
+ 
 def employee_list(request):
     employees = Employee.objects.all().order_by('name')
-    return render(request, 'employee_list.html', {'employees': employees})
+    engineers = Employee.objects.filter(position__icontains='Engineer').order_by('name')
+    return render(request, 'employee_list.html', {'employees': employees, 'engineers': engineers})
+ 
+def employee_detail(request, employee_id):
+    employee = Employee.objects.get(id=employee_id)
+    projects = Project.objects.all()
+    return render(request, 'employee_detail.html', {'employee':employee,'projects':projects})
+ 
+def employee_engineers(request):
+    engineers = Employee.objects.filter(position__icontains='Engineer')
+    return render(request, 'employee_list.html', {'engineers': engineers})
